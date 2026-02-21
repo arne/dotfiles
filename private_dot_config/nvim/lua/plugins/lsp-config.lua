c2f4f507de896988f427d06d9eb0f15bf0a34cb7
Arne Skaar Fismen <arnefismen@gmail.com>
2026-02-21 18:24:37 +0100

    pin mason-lspconfig to v1.x for nvim 0.10 compat

--- private_dot_config/nvim/lua/plugins/lsp-config.lua
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -7,6 +7,7 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    version = "1.*",
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "ts_ls" }
